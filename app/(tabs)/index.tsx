c0a0efdd720f920fa2978425a47af1411e2efddc
amine omari <omari1amineco@gmail.com>
2025-02-11 11:22:16 +0300

    test tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { HStack } from "@/components/ui/hstack";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text className="text-2xl font-bold">Home Screen</Text>
       <HStack></HStack>
     </View>
   );

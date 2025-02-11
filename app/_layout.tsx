886d6837596c324700c692c1b7a1df1acf97e69e
amine omari <omari1amineco@gmail.com>
2025-02-11 13:37:45 +0300

    feat: add glustackProvider to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,10 +31,12 @@ export default function RootLayout() {
   }
 
   return (
-    <GluestackUIProvider mode="light"><Stack>
+    <GluestackUIProvider mode="light">
+      <Stack>
         <StatusBar style="auto" />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-      </Stack></GluestackUIProvider>
+      </Stack>
+    </GluestackUIProvider>
   );
 }

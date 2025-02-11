467ae2a0a97487cb5b734f4651c587fa3bd8bd75
amine omari <omari1amineco@gmail.com>
2025-02-11 11:17:39 +0300

    import HStack

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { HStack } from "@/components/ui/hstack";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
+      <HStack></HStack>
     </View>
   );
 }

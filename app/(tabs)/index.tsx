9675f022453cb77c0aea55cf122d65b6a5764bb1
amine omari <omari1amineco@gmail.com>
2025-02-11 11:22:52 +0300

    remove style prop

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { HStack } from "@/components/ui/hstack";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View>
       <Text className="text-2xl font-bold">Home Screen</Text>
       <HStack></HStack>
     </View>

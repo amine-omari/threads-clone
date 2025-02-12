6eacfce45694bc27b55d7ad210b3f0189e129f85
amine omari <omari1amineco@gmail.com>
2025-02-12 10:47:55 +0300

    feat: add new Profile Screen

---
+++ screens/profile/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text>Search</Text>
+    </SafeAreaView>
+  );
+};

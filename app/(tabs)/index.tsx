f3ea3aff70c1419d20c59bbef1f1b856809dd42c
amine omari <omari1amineco@gmail.com>
2025-02-12 10:35:26 +0300

    feat: import Home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,2 @@
-import { SafeAreaView } from "react-native";
-import { HStack } from "@/components/ui/hstack";
-import { Text } from "@/components/ui/text";
-
-export default function HomeScreen() {
-  return (
-    <SafeAreaView>
-      <HStack className="justify-between">
-        <Text className="text-2xl font-bold">Home Screen</Text>
-        <Text className="text-2xl font-bold">Test</Text>
-      </HStack>
-    </SafeAreaView>
-  );
-}
+import Home from "@/screens/home";
+export default () => <Home />;

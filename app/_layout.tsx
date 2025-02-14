9f45a5b53980edf1c5217a20086f9e8c23a52e11
amine omari <omari1amineco@gmail.com>
2025-02-14 16:55:12 +0300

    feat: add suth to stack screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout() {
     <GluestackUIProvider mode="light">
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen
           name="post"
           options={{ headerShown: false, presentation: "modal" }}

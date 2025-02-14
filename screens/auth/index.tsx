bc99262bc363fdab2a7f452ea9438ebfefe3de86
amine omari <omari1amineco@gmail.com>
2025-02-14 16:50:44 +0300

    feat: add login screen

---
+++ screens/auth/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native";
+import { Text } from "@/components/ui/text";
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl font-bold">Auth</Text>
+    </SafeAreaView>
+  );
+};
